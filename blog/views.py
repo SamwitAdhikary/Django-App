2808a153a309e535c88b7c53a7a55ef83cdc8d22
Samwit <samwitadhikary2@gmail.com>
2020-11-08 22:29:16 +0530

    Added Pagination

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, HttpResponse
 from .models import Post
+from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 # Create your views here.
 def blog(request):
-    allPosts = Post.objects.all()
+    all_post = Paginator(Post.objects.filter(published=True).order_by('-timestamp'), 6)
+
+    page = request.GET.get('page')
+    try:
+        allPosts = all_post.page(page)
+    except PageNotAnInteger:
+        allPosts = all_post.page(1)
+    except EmptyPage:
+        allPosts = all_post.page(all_post.num_pages)
+
     context = {'allPosts': allPosts}
     return render(request, 'blog/blog.html', context)
 
