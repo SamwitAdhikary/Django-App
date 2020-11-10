32d3db6f009594eaae4de40523b320f480bd5b72
Samwit <samwitadhikary2@gmail.com>
2020-11-10 12:53:57 +0530

    Added Search and pagination

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls.conf import include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='Home'),
     path('contact', views.contact, name='Contact'),
+    path('search', views.search, name='Search'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from .models import Conatct
 from blog.models import Post
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 def index(request):
     allPosts = Post.objects.all()
@@ -21,3 +23,24 @@ def contact(request):
 
     return render(request, 'home/contact.html')
     # return HttpResponse('This is contact page')
+
+def search(request):
+    posts_list = Post.objects.all()
+    query = request.GET.get('query')
+    if query:
+        posts_list = Post.objects.filter(
+            Q(title__icontains=query) | Q(short_desc__icontains=query)
+        ).distinct()
+
+    paginator = Paginator(posts_list, 6)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    params = {'posts': posts, 'query': query}
+    return render(request, 'home/search.html', params)
