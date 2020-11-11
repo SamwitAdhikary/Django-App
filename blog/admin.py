be5d7ff0285be206dad40130623c623903ac995f
Samwit <samwitadhikary2@gmail.com>
2020-11-11 11:27:51 +0530

    Added TinyMCE

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js = ("js/tinyinject.js",)
+
+admin.site.register(Post, PostAdmin)
